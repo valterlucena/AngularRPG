a5ab94121032efa1205e194e5812283601d2b453
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2018-03-23 02:03:37 -0300

    view start implementada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+import { AppComponent } from './components/app.component';
+import { StartComponent } from './components/start/start.component';
+import { CharacterCreationComponent } from './components/character-creation/character-creation.component';
+import { StoryComponent } from './components/story/story.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { FightComponent } from './components/fight/fight.component';
 
-import { AppComponent } from './app.component';
-
+const routes: Routes = [
+  { path: "", component: StartComponent},
+  { path: "story", component: StoryComponent},
+  { path: "character-creation", component: CharacterCreationComponent},
+  { path: "fight", component: FightComponent},
+  { path: "**", redirectTo: "" }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    CharacterCreationComponent,
+    StoryComponent,
+    InventoryComponent,
+    FightComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/character-creation/character-creation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "character-creation-component",
+    templateUrl: "./character-creation.component.html",
+    styleUrls: ["./character-creation.component.css"]
+})
+export class CharacterCreationComponent {
+    
+}
