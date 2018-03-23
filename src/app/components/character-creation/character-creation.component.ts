831f1c76a0377f93acbfc8b61bf17f8b401d9ce7
Valter Lucena <valter.lucena@ccc.ufcg.edu.br>
2018-03-23 03:13:39 -0300

    criacao do personagem implementada

--- src/app/components/character-creation/character-creation.component.ts
+++ src/app/components/character-creation/character-creation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CharacterOptions } from '../../models/character-options';
 
 @Component({
     selector: "character-creation-component",
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
     styleUrls: ["./character-creation.component.css"]
 })
 export class CharacterCreationComponent {
+    character = {
+        race: '--Choose--',
+        class: '--Choose--',
+        gender: undefined,
+        name: undefined
+    }
+
+    characterComplete: boolean = false;
+
+    races = CharacterOptions.races;
+    classes = CharacterOptions.classes;
+    gender =  CharacterOptions.gender;
+
+    changeRace(race: string) {
+        this.character.race = race;
+        this.checkCompleted();
+    }
+
+    changeClass(newClass: string) {
+        this.character.class = newClass;
+        this.checkCompleted();
+    }
+
+    changeGender(gender: string) {
+        this.character.gender = gender;
+        this.checkCompleted();
+    }
     
+    changeName() {
+        this.checkCompleted();
+    }
+
+    checkCompleted() {
+        this.characterComplete = this.character.race !== "--Choose--"
+            && this.character.class !== "--Choose--"
+            && this.character.gender
+            && this.character.name
+    }
+
+    createCharacter() {
+        if(!this.characterComplete) {
+            return;
+        }
+        console.log(this.character);
+    }
+
 }
